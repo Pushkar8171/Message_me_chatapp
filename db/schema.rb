5c322f94c12022d7fe73e40ad1f107cc08aaad2d
Pushkar <pushkarsinghrathore81@gmail.com>
2023-11-28 18:03:32 +0530

    Added associations b/w the models + foreign key implementation bw models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_11_28_094226) do
+ActiveRecord::Schema.define(version: 2023_11_28_120941) do
+
+  create_table "messages", force: :cascade do |t|
+    t.string "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
